ec2373fdd5343e733fe2f6d2812704ceffb47642
Alvaroqm17 <92014738+Alvaroqm17@users.noreply.github.com>
2022-12-16 12:24:19 +0100

    altura campanas

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ var estrella = document.getElementById("estrella");
 var campana1 = document.getElementById("campana1");
 var campana2 = document.getElementById("campana2");
 //IYECTAMOS ESTILOS CSS A ELEMENTOS DE LA POSTAL
-campana2.style.cssText=" display: flex;position: absolute;height: 150px;width: 150px;margin-bottom: 80vh;margin-left: 40%;display: flex;position: absolute;animation-name: rotate;animation-duration: 1.5s;animation-iteration-count: infinite;";
-campana1.style.cssText="display: flex;position: absolute;height: 150px;width: 150px;margin-bottom: 80vh;margin-right: 40%;display: flex;position: absolute;animation-name: spin;animation-duration: 1.5s;animation-iteration-count: infinite;";
+campana2.style.cssText=" display: flex;position: absolute;height: 150px;width: 150px;margin-bottom: 90vh;margin-left: 40%;display: flex;position: absolute;animation-name: rotate;animation-duration: 1.5s;animation-iteration-count: infinite;";
+campana1.style.cssText="display: flex;position: absolute;height: 150px;width: 150px;margin-bottom: 90vh;margin-right: 40%;display: flex;position: absolute;animation-name: spin;animation-duration: 1.5s;animation-iteration-count: infinite;";
 estrella.style.cssText = "height: 100px; width: 100px;  margin-bottom: 100px;position: absolute; animation-name: parpadeo; animation-duration: 1s; animation-timing-function: linear; animation-iteration-count: infinite; -webkit-animation-name:parpadeo; -webkit-animation-duration: 1s; -webkit-animation-timing-function: linear; -webkit-animation-iteration-count: infinite;";
 //FUNCION QUE HACE VISIBLE LA FELICITACION Y EL NOMBRE
 function mostrarSaludo() {
